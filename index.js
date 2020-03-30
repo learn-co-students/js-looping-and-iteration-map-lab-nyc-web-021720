6ab49484b9af18dc4c734e9941de89888d2771c9
JazzyMussels <jeffmussels@gmail.com>
2020-03-29 22:38:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file.
+function lowerCaseDrivers(arr){
+    return arr.map( x => x.toLowerCase())
+}
+
+function nameToAttributes(arr){
+   return  arr.map( x => {
+      return {firstName: x.split(' ')[0], lastName: x.split(' ')[1]}
+   })
+}
+
+function attributesToPhrase(arr){
+    return arr.map(x => `${x.name} is from ${x.hometown}`)
+}
